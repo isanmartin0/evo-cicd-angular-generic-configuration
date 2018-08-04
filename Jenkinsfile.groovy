4d58385873afa40da9785c103ba3d2000ac0074c
isanmartin <isanmartin@keedio.com>
2018-08-04 11:12:49 +0200

    Modified Angular pipeline

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -350,8 +350,8 @@ def runAngularGenericJenkinsfile() {
             }
 
             stage('Configure Artifactory NPM Registry') {
-                echo 'Setting Artifactory NPM registry'
-                sh "npm config set registry ${npmRepositoryURL} "
+                //echo 'Setting Artifactory NPM registry'
+                //sh "npm config set registry ${npmRepositoryURL} "
             }
 
             stage('Install @angular/cli') {
