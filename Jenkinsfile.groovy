290c6be3ea3724db22c3ae2cf64c233e7bdd5bbc
isanmartin <isanmartin@keedio.com>
2018-08-16 13:48:37 +0200

    Modified Angular pipeline

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -106,8 +106,8 @@ def runAngularGenericJenkinsfile() {
     echo "BEGIN ANGULAR GENERIC CONFIGURATION PROJECT (PGC)"
 
     //node('nodejs10-chrome') {
-    node('nodejs') {
-    //node('prueba-nodejs-centos') {
+    //node('nodejs') {
+    node('prueba-nodejs-centos') {
 
         echo 'Pipeline begin timestamp... '
         sh 'date'
