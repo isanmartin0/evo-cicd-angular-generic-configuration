069bf5a9416312af163b33612bbe446ef78bdb68
isanmartin <isanmartin@keedio.com>
2018-10-30 17:55:11 +0100

    Change npm virtual artifactory repo

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,7 +7,7 @@ def runAngularGenericJenkinsfile() {
     AngularUtils utils = new AngularUtils()
 
     //def angularNPMRepositoryURL = 'https://digitalservices.evobanco.com/artifactory/api/npm/angular-npm-repo/'
-    def angularNPMRepositoryURL = 'https://digitalservices.evobanco.com/artifactory/api/npm/npm-remote-prueba-cache/'
+    def angularNPMRepositoryURL = 'https://digitalservices.evobanco.com/artifactory/api/npm/npm-remote-prueba/'
 
     def angularNPMLocalRepositoryURL = 'https://digitalservices.evobanco.com/artifactory/api/npm/angular-npm-local/'
     def angularGenericLocalRepositoryURL = 'https://digitalservices.evobanco.com/artifactory/angular-generic-local/'
